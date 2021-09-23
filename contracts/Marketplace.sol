7a305b2d7c2271eec5e1afd2c537a5192e2232f5
Jose Ignacio Naranjo <52728126+123-code@users.noreply.github.com>
2021-09-23 16:37:37 -0500

    Fixed Contract Mistakes

--- contracts/Marketplace.sol
+++ contracts/Marketplace.sol
@@ -25,14 +25,56 @@ event ProductAdded(
 );
 
 modifier name_length(string memory _name){
-require(name.length > 0,"cannot be an empty name");
+    require(_name.length > 0, "Name cannot be empty!"); _;
 }
 
 modifier valid_price(uint _price){
-    require(price > 0, "price entered is not valid");
-}
+    require(Product.price > 0, "price entered is not valid");
+_; }
 
 event productpurchased(
+    uint id,
+    string name,
+    uint price,
+    address payable owner,
+    bool purchased
+);
+
+function list_product(string memory _name, uint _price )public name_length(Product.name) valid_price(Product.price){
+    ProductCount ++;
+
+// appending each product listed to an id using a mapping.
+    products[ProductCount] = Product(ProductCount,_name,_price,msg.sender,false);
+
+emit ProductAdded(ProductCount,_name,_price,msg.sender,false);
+
+}
+
+
+function buyproducts(uint _id)public payable {
+    Product memory _product = products[_id];
+
+    address  payable seller = _product.owner;
+require(_product.id  > 0 && _product.id < ProductCount);
+require(msg.value == Product.price);
+_product.owner = msg.sender;
+
+_product.purchased = true;
+
+products[_id] = _product;
+
+address(seller).transfer(msg.value);
+
+emit productpurchased(ProductCount,Product.name,Product.price,msg.sender,false);
+}
+
+constructor()public{
+name = "Token Powered Marketplece";
+}
+
+
+
+}
     uint id,
     string name,
     uint price,
